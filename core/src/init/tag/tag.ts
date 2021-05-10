2f60596634d1fae66e01b010a2527edc56bc8536
nhi.ly <nhi.ly@BeT-LMP2017-A036.local>
2021-05-10 17:29:59 +0700

    update tag colors

---
+++ core/src/init/tag/tag.ts
@@ -0,0 +1,9 @@
+import { Tag } from "@moai/core";
+import newColors from "./colors.module.css";
+
+Tag.colors.gray = newColors.gray;
+Tag.colors.green = newColors.green;
+Tag.colors.red = newColors.red;
+Tag.colors.blue = newColors.blue;
+Tag.colors.yellow = newColors.yellow;
+Tag.colors.purple = newColors.purple;
