a76bde9cb9464a0924a9bc5f6e64e81e8335e28e
violeine <haluudang@gmail.com>
2021-05-14 14:59:57 +0700

    Fix(core): Toast Design            Fix padding            Use moai's color scale instead

--- core/src/init/toast/toast.ts
+++ core/src/init/toast/toast.ts
@@ -1,16 +1,17 @@
-import { ToastPane, text, toast, ToastPaneType } from "@moai/core";
+import { ToastPane, text, ToastPaneType } from "@moai/core";
 import { Check, Warning2 } from "@banhmi/icon/solid";
 import s from "./toast.module.css";
+
 ToastPane.types = {
 	success: {
 		iconComponent: Check,
 		className: s.success,
-		iconCls: text.successStrong,
+		iconCls: text.successWeak,
 	} as ToastPaneType,
 	failure: {
 		iconComponent: Warning2,
 		className: s.failure,
-		iconCls: text.failureStrong,
+		iconCls: text.failureWeak,
 	} as ToastPaneType,
 };
 
