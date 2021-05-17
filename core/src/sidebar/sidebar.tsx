026957c6cdbd7fc0e0fdfe69302f160a6454f4e6
NgocNhi123 <lnnhi18@apcs.vn>
2021-05-17 16:54:40 +0700

    sidebar header

--- core/src/sidebar/sidebar.tsx
+++ core/src/sidebar/sidebar.tsx
@@ -1,1 +1,18 @@
-export const Sidebar = (): JSX.Element => <div>Coming soon</div>;
+import s from "./sidebar.module.css";
+import React from "react";
+
+export const Header = (props: {
+	picture: React.ReactNode;
+	name: string;
+	role: string;
+}): JSX.Element => {
+	return (
+		<div className={s.header}>
+			<div className={s.picture}>{props.picture}</div>
+			<div>
+				<div className={s.username}>{props.name}</div>
+				<div className={s.role}>{props.role}</div>
+			</div>
+		</div>
+	);
+};
