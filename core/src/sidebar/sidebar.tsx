1529faf6ad0caa236352a2529b270761b35c7d93
NgocNhi123 <lnnhi18@apcs.vn>
2021-05-19 11:26:39 +0700

    header

--- core/src/sidebar/sidebar.tsx
+++ core/src/sidebar/sidebar.tsx
@@ -2,16 +2,16 @@ import s from "./sidebar.module.css";
 import React from "react";
 
 export const Header = (props: {
-	picture: React.ReactNode;
-	name: string;
-	role: string;
+	userImg: React.ReactNode;
+	username: string;
+	userRole: string;
 }): JSX.Element => {
 	return (
 		<div className={s.header}>
-			<div className={s.picture}>{props.picture}</div>
-			<div>
-				<div className={s.username}>{props.name}</div>
-				<div className={s.role}>{props.role}</div>
+			<div className={s.picture}>{props.userImg}</div>
+			<div className={s.right}>
+				<div className={s.username}>{props.username}</div>
+				<div className={s.role}>{props.userRole}</div>
 			</div>
 		</div>
 	);
