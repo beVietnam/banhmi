5f1dc426cb7731ad8e4f6893d6cb39e328ccca46
NgocNhi123 <lnnhi18@apcs.vn>
2021-05-20 10:30:48 +0700

    fix some weird behavior of active menu

--- core/src/sidebar/collapse/collapse.tsx
+++ core/src/sidebar/collapse/collapse.tsx
@@ -1,43 +1,46 @@
-import {Menu, WrappedProps as ChildWrapperProps} from "../menu/menu"
-import {IMenu} from "../common"
-import { useState } from "react"
+import { Menu, WrappedProps as ChildWrapperProps } from "../menu/menu";
+import { IMenu } from "../common";
+import { useEffect, useState } from "react";
 
 export const Collapse = (
 	props: {
 		menu: IMenu;
 		path: string;
-        setPath: (path: string) => void;
+		setPath: (path: string) => void;
 	} & ChildWrapperProps
-):JSX.Element => {
+): JSX.Element => {
 	const [isActive, setActive] = useState(props.menu.isActive);
-	return <div>
-		<div onClick={() => setActive(!isActive)}>
-			<Menu menu={{
-				title: props.menu.title,
-				path: props.menu.path,
-				after: props.menu.after,
-				iconActive: props.menu.iconActive,
-				iconNormal: props.menu.iconNormal,
-				isActive: isActive,
-				isChild: props.menu.isChild,
-				childrens: props.menu.childrens,
-			}}/>
-		</div>
-		{isActive && props.menu.childrens && props.menu.childrens.map((item) => {
-			const active = props.path.includes(item.path);
-			return <div key={item.path} onClick={() => props.setPath(item.path)}>
-				<Menu menu={{
-				title: item.title,
-				path: item.path,
-				after: item.after,
-				iconActive: item.iconActive,
-				iconNormal: item.iconNormal,
-				isActive: active,
-				isChild: true,
-				childrens: item.childrens,
-			}}
-			as={props.as}/>
+
+	function toggleCollapse() {
+		setActive(!isActive);
+		if (!isActive && props.path.includes(props.menu.path)) {
+			props.setPath("/");
+		}
+	}
+
+	return (
+		<div>
+			<div onClick={() => toggleCollapse()}>
+				{(props.menu.isActive = isActive)}
+				<Menu menu={props.menu} />
 			</div>
-		})}
-	</div>
-}
+			{isActive &&
+				props.menu.childrens &&
+				props.menu.childrens.map((item) => {
+					const active = props.path.includes(item.path);
+					return (
+						<div
+							key={item.path}
+							onClick={() =>
+								props.setPath(props.menu.path + item.path)
+							}
+						>
+							{(item.isActive = active)}
+							{(item.isChild = true)}
+							<Menu menu={item} as={props.as} />
+						</div>
+					);
+				})}
+		</div>
+	);
+};
