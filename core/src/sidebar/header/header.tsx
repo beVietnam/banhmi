d18bc03b773a2208f555c2d8f3f1a8c02ffe42cc
NgocNhi123 <lnnhi18@apcs.vn>
2021-05-24 11:10:32 +0700

    complete sidebar + create component footer

--- core/src/sidebar/header/header.tsx
+++ core/src/sidebar/header/header.tsx
@@ -1,5 +1,4 @@
 import s from "./header.module.css";
-import React from "react";
 import { IUserInfo } from "../common";
 
 export const Header = (props: { userInfo: IUserInfo }): JSX.Element => {
--- core/src/sidebar/sidebar.tsx
+++ core/src/sidebar/sidebar.tsx
@@ -1,1 +1,44 @@
-export const sidebar = (): JSX.Element => <div></div>;
+import { useState } from "react";
+import { Collapse } from "./collapse/collapse";
+import { Header } from "./header/header";
+import { Footer } from "./footer/footer";
+import { Heading } from "./heading/heading";
+import s from "./sidebar.module.css";
+import { IMenu, IUserInfo, ISection } from "./common";
+
+export const Sidebar = (props: {
+	headerData: IUserInfo;
+	bodyData: ISection[];
+	footerData: IMenu[];
+}): JSX.Element => {
+	const [path, setPath] = useState("/");
+	return (
+		<div className={s.container}>
+			{/* Header */}
+			<Header userInfo={props.headerData} />
+
+			{/* Body */}
+			{props.bodyData.map((value, index) => {
+				return (
+					<div key={index}>
+						<Heading>{value.title}</Heading>
+						{value.data.map((menu, menuIndex) => {
+							return (
+								<Collapse
+									key={menuIndex}
+									menu={menu}
+									path={path}
+									setPath={setPath}
+									as="a"
+								/>
+							);
+						})}
+					</div>
+				);
+			})}
+
+			{/* Footer */}
+			<Footer data={props.footerData} />
+		</div>
+	);
+};
